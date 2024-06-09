823ede471311bc1d213793fbbb97cd2c44cb554d
Christian Thäter <ct@pipapo.org>
2024-06-10 01:17:48 +0200

    rewrite the `unsynn!` macro as tt-muncher

--- src/macros.rs
+++ src/macros.rs
@@ -4,163 +4,151 @@ use crate::*;
 /// Construct types with a [`Parser`] and [`ToTokens`] implementation that will try to
 /// parse/generate each entity in order. Will also implement `Debug` and `Display` if the
 /// `impl_debug` and `impl_display` features are enabled. This macro supports enums, tuple
-/// structs and normal structs. Generics/Lifetimes are not supported (yet) and all entities in
-/// a single `unsynn!` invocation have to be of the same kind. Note: eventually a derive macro
-/// for `Parser` and `ToTokens` will become supported by a 'unsynn-derive' crate to give
-/// finer control over the expansion.
+/// structs and normal structs. Generics/Lifetimes are not supported (yet). Note: eventually a
+/// derive macro for `Parser` and `ToTokens` will become supported by a 'unsynn-derive' crate
+/// to give finer control over the expansion.
 ///
 /// Common for all three variants is that entries are tried in order. Disjunctive for enums
 /// and conjunctive in structures.
 ///
-/// # Examples
+/// # Example
 ///
-/// * Enum
 /// ```
 /// # use unsynn::*;
+/// // Define all required types
 /// unsynn!{
+///     // Enum
 ///     enum MyEnum {
 ///         Ident(Ident),
 ///         Braced(BraceGroup),
 ///         Text(LiteralString),
 ///         Number(LiteralInteger),
 ///     }
-/// }
 ///
-/// let mut token_iter = quote::quote! { ident { within brace } "literal string" 1234}.into_iter();
-///
-/// let MyEnum::Ident(_) = MyEnum::parse(&mut token_iter).unwrap() else { panic!()};
-/// let MyEnum::Braced(_) = MyEnum::parse(&mut token_iter).unwrap() else { panic!()};
-/// let MyEnum::Text(_) = MyEnum::parse(&mut token_iter).unwrap() else { panic!()};
-/// let MyEnum::Number(_) = MyEnum::parse(&mut token_iter).unwrap() else { panic!()};
-/// ```
-///
-/// * Struct
-/// ```
-/// # use unsynn::*;
-/// unsynn!{
+///     // Struct
 ///     struct MyStruct {
 ///         text: LiteralString,
 ///         number: LiteralInteger,
 ///     }
-/// }
 ///
-/// let mut token_iter = quote::quote! { "literal string" 1234 }.into_iter();
-///
-/// let my_struct =  MyStruct::parser(&mut token_iter).unwrap();
-/// ```
-///
-/// * Tuple Struct
-/// ```
-/// # use unsynn::*;
-/// unsynn!{
+///     // Tuple Struct
 ///     struct MyTupleStruct(Ident, LiteralString);
 /// }
 ///
-/// let mut token_iter = quote::quote! { ident "literal string"}.into_iter();
+/// // Use the defined types
+/// let mut token_iter = quote::quote! { ident { within brace } "literal string" 1234}.into_iter();
+/// let MyEnum::Ident(_) = MyEnum::parse(&mut token_iter).unwrap() else { panic!()};
+/// let MyEnum::Braced(_) = MyEnum::parse(&mut token_iter).unwrap() else { panic!()};
+/// let MyEnum::Text(_) = MyEnum::parse(&mut token_iter).unwrap() else { panic!()};
+/// let MyEnum::Number(_) = MyEnum::parse(&mut token_iter).unwrap() else { panic!()};
 ///
+/// let mut token_iter = quote::quote! { "literal string" 1234 }.into_iter();
+/// let my_struct =  MyStruct::parser(&mut token_iter).unwrap();
+///
+/// let mut token_iter = quote::quote! { ident "literal string"}.into_iter();
 /// let my_tuple_struct =  MyTupleStruct::parser(&mut token_iter).unwrap();
 /// ```
 #[macro_export]
 macro_rules! unsynn{
-    ($($(#[$attribute:meta])* $pub:vis enum $name:ident { $($variant:ident($parse:ty)),* $(,)? })*) => {
-        $(
-            #[cfg_attr(feature = "impl_debug", derive(Debug))]
-            $(#[$attribute])* $pub enum $name {
-                $($variant($parse)),*
-            }
+    ($(#[$attribute:meta])* $pub:vis enum $name:ident { $($variant:ident($parse:ty)),* $(,)? } $($cont:tt)*) => {
+        #[cfg_attr(feature = "impl_debug", derive(Debug))]
+        $(#[$attribute])* $pub enum $name {
+            $($variant($parse)),*
+        }
 
-            impl Parser for $name {
-                fn parser(tokens: &mut TokenIter) -> Result<Self> {
+        impl Parser for $name {
+            fn parser(tokens: &mut TokenIter) -> Result<Self> {
+                $(
+                    if let Ok(parsed) = <$parse>::parse(tokens) {
+                        return Ok($name::$variant(parsed));
+                    }
+                )*
+                    match tokens.next() {
+                        Some(token) => $crate::Error::unexpected_token(token),
+                        None => $crate::Error::unexpected_end()
+                    }
+            }
+        }
+
+        impl ToTokens for $name {
+            fn to_tokens(&self, tokens: &mut TokenStream) {
+                match self {
                     $(
-                        if let Ok(parsed) = <$parse>::parse(tokens) {
-                            return Ok($name::$variant(parsed));
-                        }
+                        $name::$variant(matched) => matched.to_tokens(tokens),
                     )*
-                        match tokens.next() {
-                            Some(token) => $crate::Error::unexpected_token(token),
-                            None => $crate::Error::unexpected_end()
-                        }
                 }
             }
+        }
 
-            impl ToTokens for $name {
-                fn to_tokens(&self, tokens: &mut TokenStream) {
-                    match self {
-                        $(
-                            $name::$variant(matched) => matched.to_tokens(tokens),
-                        )*
-                    }
+        #[cfg(feature = "impl_display")]
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                match self {
+                    $(
+                        $name::$variant(matched) => write!(f, "{matched} "),
+                    )*
                 }
             }
-
-            #[cfg(feature = "impl_display")]
-            impl std::fmt::Display for $name {
-                fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                    match self {
-                        $(
-                            $name::$variant(matched) => write!(f, "{matched} "),
-                        )*
-                    }
-                }
-            }
-        )*
+        }
+        $crate::unsynn!{$($cont)*}
     };
-    ($($(#[$attribute:meta])* $pub:vis struct $name:ident { $($mpub:vis $member:ident: $parse:ty),* $(,)? })*) => {
-        $(
-            #[cfg_attr(feature = "impl_debug", derive(Debug))]
-            $(#[$attribute])* $pub struct $name {
-                $($mpub $member : $parse),*
-            }
+    ($(#[$attribute:meta])* $pub:vis struct $name:ident { $($mpub:vis $member:ident: $parse:ty),* $(,)? } $($cont:tt)*) => {
+        #[cfg_attr(feature = "impl_debug", derive(Debug))]
+        $(#[$attribute])* $pub struct $name {
+            $($mpub $member : $parse),*
+        }
 
-            impl Parser for $name {
-                fn parser(tokens: &mut TokenIter) -> Result<Self> {
-                    Ok(Self{$($member: <$parse>::parser(tokens)?),*})
-                }
+        impl Parser for $name {
+            fn parser(tokens: &mut TokenIter) -> Result<Self> {
+                Ok(Self{$($member: <$parse>::parser(tokens)?),*})
             }
+        }
 
-            impl ToTokens for $name {
-                fn to_tokens(&self, tokens: &mut TokenStream) {
-                    $(self.$member.to_tokens(tokens);)*
-                }
+        impl ToTokens for $name {
+            fn to_tokens(&self, tokens: &mut TokenStream) {
+                $(self.$member.to_tokens(tokens);)*
             }
+        }
 
-            #[cfg(feature = "impl_display")]
-            impl std::fmt::Display for $name {
-                fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                    $(write!(f, "{} ", &self.$member);)*
-                        Ok(())
-                }
-            }
-        )*
-    };
-    ($($(#[$attribute:meta])* $pub:vis struct $name:ident ($($mpub:vis $parse:ty),* $(,)?);)*) => {
-        $(
-            #[cfg_attr(feature = "impl_debug", derive(Debug))]
-            $(#[$attribute])* $pub struct $name (
-                $($mpub $parse),*
-            );
-
-            impl Parser for $name {
-                fn parser(tokens: &mut TokenIter) -> Result<Self> {
-                    Ok(Self($(<$parse>::parser(tokens)?),*))
-                }
-            }
-
-            impl ToTokens for $name {
-                fn to_tokens(&self, tokens: &mut TokenStream) {
-                    $crate::unsynn!{@tuple_to_tokens $name(self, tokens) $($parse),*}
-                }
-            }
-
-            #[cfg(feature = "impl_display")]
-            impl std::fmt::Display for $name {
-                fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                    $crate::unsynn!{@tuple_write $name(self, f) $($parse),*}
+        #[cfg(feature = "impl_display")]
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                $(write!(f, "{} ", &self.$member);)*
                     Ok(())
-                }
             }
-        )*
+        }
+        $crate::unsynn!{$($cont)*}
     };
+    ($(#[$attribute:meta])* $pub:vis struct $name:ident ($($mpub:vis $parse:ty),* $(,)?); $($cont:tt)*) => {
+        #[cfg_attr(feature = "impl_debug", derive(Debug))]
+        $(#[$attribute])* $pub struct $name (
+            $($mpub $parse),*
+        );
+
+        impl Parser for $name {
+            fn parser(tokens: &mut TokenIter) -> Result<Self> {
+                Ok(Self($(<$parse>::parser(tokens)?),*))
+            }
+        }
+
+        impl ToTokens for $name {
+            fn to_tokens(&self, tokens: &mut TokenStream) {
+                $crate::unsynn!{@tuple_to_tokens $name(self, tokens) $($parse),*}
+            }
+        }
+
+        #[cfg(feature = "impl_display")]
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                $crate::unsynn!{@tuple_write $name(self, f) $($parse),*}
+                Ok(())
+            }
+        }
+        $crate::unsynn!{$($cont)*}
+    };
+
+    // terminate recursion
+    () => {};
 
     // For the tuple struct ToTokens impl we need to match each tuple member and call to_tokens on it
     (@tuple_to_tokens $name:ident($this:ident,$param:ident) $element:ty $(,$rest:ty)* $(,)?) => {
@@ -178,8 +166,8 @@ macro_rules! unsynn{
     };
     (@tuple_write $name:ident($this:ident,$f:ident)) => {};
 
-    // replaces a ty with a underscore
-    (@_ $unused:ty) => {_};
+    // replaces a single token with a underscore
+    (@_ $unused:tt) => {_};
 }
 
 /// Define types matching keywords.
