e52b4626fa446e40259c74d4b11dce786eefd3a0
Christian Thäter <ct@pipapo.org>
2024-09-20 16:25:45 +0200

    add testcase which shows a rust-analyzer bug

--- tests/macro_tests.rs
+++ tests/macro_tests.rs
@@ -6,15 +6,15 @@ unsynn! {
         One(Plus, Dot),
         TwoS{ a: Minus, b: Minus, c: Dot},
         OneS{ a: Minus, b: Dot},
+        // the Expect<Dollar> shows a rust-analyzer error here, which is probably a bug
+        PunctBreak(Punct, Expect<Dollar>),
     }
-
-    //struct Either(Either<Cons<Plus,Plus, Dot>, Cons<Plus, Dot>>);
 }
 
 // Bug in 0.0.17, parsing Enum::Two consumes the Plus token
 #[test]
 fn test_parse_enum_consume_bug() {
-    let mut i = "+.-.".to_token_iter();
+    let mut i = "+.-.*$".to_token_iter();
 
     let parsed = i.parse::<Enum>().unwrap();
     assert!(matches!(parsed, Enum::One(..)));
@@ -23,4 +23,16 @@ fn test_parse_enum_consume_bug() {
     let parsed = i.parse::<Enum>().unwrap();
     assert!(matches!(parsed, Enum::OneS { .. }));
     assert_eq!(parsed.tokens_to_string(), "- .".tokens_to_string());
+}
+
+#[test]
+fn test_generics() {
+    let mut i = "*$".to_token_iter();
+
+    let parsed = i.parse::<Enum>().unwrap();
+    assert!(matches!(parsed, Enum::PunctBreak(..)));
+    assert_eq!(parsed.tokens_to_string(), "*".tokens_to_string());
+
+    let parsed = i.parse::<Dollar>().unwrap();
+    assert_eq!(parsed.tokens_to_string(), "$".tokens_to_string());
 }
